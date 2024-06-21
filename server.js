2a0f39c208835f102ebdb5fc63cde1e174f2a0d8
Jennie <dwkang0526@gmail.com>
2024-06-21 11:06:53 +0900

    부동산 매매 상세 화면

--- server.js
+++ server.js
@@ -2,18 +2,24 @@
 
 //import
 const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 var mysql = require("mysql");
 
 var conn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
-    database: "myboard",
+    password: "root1234",
+    database: "workshopBankWeb",
 });
 
-conn.connect();
+conn.connect(function(err){
+    if(err){
+        console.log("Mysql 연결 실패 :: "+ err);
+    }else{
+        console.log("Mysql 연결 성공");
+    }
+});
+
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -21,7 +27,26 @@ app.use(express.static('static'));
 
 
 //api부
-
 app.listen(8080, function(){
     console.log('server ready');
+});
+
+// 게시물 조회
+app.get('/landPage/:id', (req, res) => {
+    // TEST ========= TODO : 나중에 수정 후 삭제
+    const id = req.params.id;
+    // const id = 3;
+
+    const sql = `SELECT * FROM land WHERE land_id = ${id}`;
+    
+    conn.query(sql, function(err, rows, fields){
+        if(err){
+            console.log("Query Error");
+            throw err;
+        }else{
+            console.log(rows);
+            res.render('landDetail.ejs', {data:rows});
+        }
+    });
+
 });
